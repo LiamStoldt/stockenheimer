7e71a95efd8c1829dea1e3686adb052622ad39a4
Liam Stoldt <liamstoldt@yahoo.com>
2023-08-09 20:03:37 +0930

    image output

--- stockenheimer.py
+++ stockenheimer.py
@@ -1,7 +1,7 @@
 """Main flask file for stockenheimer project"""
 
 from flask import Flask
-
+from asx_price_getter import get_stock_daily_change
 
 app = Flask(__name__)
 
@@ -12,4 +12,23 @@ def index():
     
     return "<p>Hello world!</p>"
 
+
+@ app.route("/<string:ticker>")
+def stock_page(ticker: str):
+    """Stock page, displays the meme"""
+
+    # perform stock test
+    # need to escape the input string :)
+    happy = True
+
+
+    image_path = ""
+    if (happy):
+        image_path = "images/happyheimer.jpg"
+    else:
+        image_path = "images/sadenheimer.png"
+    # need to have these images the same type for my sake
+
+    
+    return f'<img src="{image_path}" alt="oppenheimer">'
 
